c9c48f6800a68e3dce4acdd8c501a9bbf709bc8e
dannielsss <gamekik92@gmail.com>
2023-08-12 11:29:53 -0600

    feat: :dizzy: add simple animations

--- src/hooks/useCalculator.tsx
+++ src/hooks/useCalculator.tsx
@@ -1,4 +1,4 @@
-import { useState, MouseEvent } from 'react';
+import { useState, MouseEvent, useEffect, useCallback } from 'react';
 
 /**
  * Hook for calculator events
@@ -36,14 +36,52 @@ export const useCalculator = () => {
   /**
    * Function to get the result
    */
-  const handleResult = () => {
+  const handleResult = useCallback(() => {
     try {
       const result = String(eval(screenValue.replace('x', '*')));
       setScreenValue(result);
     } catch (error) {
       setScreenValue('Syntax error');
     }
-  };
+  }, [screenValue]);
+
+  /**
+   * To handle keyboard events
+   */
+  useEffect(() => {
+    const handleKeyDown = (event: globalThis.KeyboardEvent) => {
+      if (event.key === 'Escape') return handleReset();
+      if (event.key === 'Backspace') return handleDeleteCharacter();
+      if (event.key === 'Enter') return handleResult();
+
+      const keysNumbers = [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '0',
+        '-',
+        '+',
+        'x',
+        '/',
+        '.',
+      ];
+
+      keysNumbers.forEach((key) => {
+        if (event.key === key) return setScreenValue((prev) => prev + key);
+      });
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleResult]);
 
   return {
     screenValue,
